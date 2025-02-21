ce9b76f9a8c6c9b98b90a652296be7db33b67560
Guilherme Leite <141077982+guigh@users.noreply.github.com>
2025-02-21 17:19:53 -0400

    projeto pig
    aaa gay

---
+++ script.js
@@ -0,0 +1,8 @@
+// Rolagem pra seções
+document.getElementById('sobre').addEventListener('click', () => {
+    document.getElementById('sobrepig').scrollIntoView({ behavior: 'smooth' });
+  });
+  
+  document.getElementById('contato').addEventListener('click', () => {
+    document.getElementById('contatopig').scrollIntoView({ behavior: 'smooth' });
+  });
